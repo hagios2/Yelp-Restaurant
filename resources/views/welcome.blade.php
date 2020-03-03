f0fea5aa121498d1e815d9316b4ddda425c98857
hagios <hagioswilson@gmail.com>
2020-03-03 11:31:56 +0000

    card modification

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -457,11 +457,11 @@ paginationVars.totalNumberOfPage = Math.ceil(paginationVars.paginationDataLength
                 {
                     console.log(data);
 
-                    $('#main_businessDiv').append('<br><div id="businessDiv '+ i+'"></div>'); 
+                    $('#main_businessDiv').append('<div id="businessDiv'+ i+'"></div>'); 
 
                    /*  $.each(data, function(i, data){ */
 
-                        $('#businessDiv'+i).append('<div id="main-card '+i+'" class="card">');
+                        $('#businessDiv'+i).append('<div id="main-card'+i+'" class="card"></div>');
 
                         $('#main-card' +i).append('<div class="card-title"><h3>'+ data.name +' </h3></div>');
 
@@ -474,14 +474,14 @@ paginationVars.totalNumberOfPage = Math.ceil(paginationVars.paginationDataLength
                           cords : data.coordinates.latitude +', '+ data.coordinates.longitude,
                         };
 
-                        $('#main-card' +i).append('<div class="card-img-'+i+'">');//carousel
+                        $('#main-card' +i).append('<div class="card-img-'+i+'"></div>');//carousel
 
-                        $('.card-img'+i).append('<div id="carouselExampleIndicators" class="carousel slide car'+i+'" data-ride="carousel">' +
+                        $('.card-img-'+i).append('<div id="carouselExampleIndicators" class="carousel slide car'+i+'" data-ride="carousel">' +
                         '<ol id="carol_ind'+i+'" class="carousel-indicators"></ol>');
                         
                         $('#carol_ind'+i).append('<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>');
 
-                        $('.car'+i).append('<div class="carousel-inner"><div class="carousel-item active"><img class="d-block w-100" src="'+ data.image_ul+'" alt="First slide"></div><'); //set slider image to image_url
+                        $('.car'+i).append('<div id="carouselInner'+i+'" class="carousel-inner"><div class="carousel-item active"><img class="d-block w-100" src="'+ data.image_ul+'" alt="First slide"></div><'); //set slider image to image_url
                        
                         let carousel_id = 1;
                           
@@ -489,28 +489,27 @@ paginationVars.totalNumberOfPage = Math.ceil(paginationVars.paginationDataLength
                       
                           $('#carol_ind'+i).append('<li data-target="#carouselExampleIndicators" data-slide-to="'+carousel_id+'"></li>');
 
-                          $('#carousel-inner').append('<div class="carousel-item"><img class="d-block w-100 card-image-top" style="max-height:30rem;" src="'+pix+'" alt="Second slide">');
+                          $('#carouselInner'+i).append('<div class="carousel-item"><img class="d-block w-100 card-image-top" style="max-height:30rem;" src="'+pix+'" alt="Second slide">');
 
                           carousel_id ++;
                           
                         });
                                 
-                        $('#main-card'+i).append('<div id="card-body'+i+'" class="card-body">');
+                        $('#main-card'+i).append('<div id="card-body'+i+'" class="card-body"></div>');
 
-                        $('#card-body'+i).append('<div id="card-body-row'+i+'" class="row">');
+                        $('#card-body'+i).append('<div id="card-body-row'+i+'" class="row"></div>');
 
                         $('#card-body-row'+i).append('<div class="col-md-6 col-lg-6">'+'<i class="fas fa-phone"></i> '+data.phone+'<pReviews: '+data.review_count+'></p></div>');
 
                         $('#card-body-row'+i).append('<div class="class="col-md-4 col-lg-4 offset-5"><iframe width="100%" height="100" src="https://maps.google.com/maps?width=100%&amp;height=100&amp;hl=en&amp;coord='+restaurant.cords+'q=+('+restaurant.name+')&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/coordinates.html">latitude longitude finder</a></iframe></div><br />');
 
-                        $('#card-body-row'+i).append('</div>');//end row
-
-                        $('#main-card'+i).append('</div>');//end class card-body
-
-                        $('#main-card'+i).append('</div> <br>');//end class card
+                        /* $('#card-body-row'+i).append('</div>');//end row */
+/* 
+                        $('#main-card'+i).append('</div>'); *///end class card-body
+/* 
+                        $('#main-card'+i).append('</div> <br></br>'); *///end class card
                   
                     //});
-
 
               }
  
